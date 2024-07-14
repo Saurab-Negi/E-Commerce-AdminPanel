7b0ec8f127edf54a62ed67e859728b613b9d7301
Saurab-Negi <saurabnegi17@gmail.com>
2024-07-14 20:49:03 +0530

    Products added

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import list_product_icon from '../../assets/Product_list_icon.svg'
 
 const Sidebar = () => {
   return (
-    <div className='sidebar flex flex-col m-2 py-4 px-2 gap-2 w-full max-w-[15rem] h-[100vh] rounded-xl bg-white '>
+    <div className='sidebar flex flex-col m-2 py-4 px-2 gap-2 w-full max-w-[15rem] h-screen rounded-xl bg-white '>
       
         <Link to={'/product/addproduct'} style={{textDecoration:"none"}}>
             <div className="sidebar-item">
